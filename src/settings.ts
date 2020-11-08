ed7be4a587dea55fce033543886b979b5a189731
Jeremy Dunn <innerspacesuit@gmail.com>
2020-11-08 11:47:26 -0600

    update settings, package.json

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+export const PLUGIN_NAME = 'homebridge-govee-water-detectors';
